223be999428e35bc8d8f1e5fba97a8771670956a
Danger <51889207+DangerDan88@users.noreply.github.com>
2019-09-17 15:18:54 -0600

    first commit for trivia game

---
+++ assets/javascript/app.js
@@ -0,0 +1,29 @@
+ var quizContainer = $("#gameContainer");
+ var resultsContainer = $("#results");
+var startButton = document.getElementById("start");
+var myQuestions = [
+    {
+        question: "Which NBA franchise has the most championships?",
+        answers: {
+          a: "Lakers",
+          b: "Jazz",
+          c: "Celtics"
+        } , 
+        
+    },
+    {
+        correctAnswer: "c"
+    }
+
+]
+
+window.onload = function() {
+   document.getElementById("start").addEventListener("click", function() {
+          
+   })
+
+    
+
+  }
+  
+  
