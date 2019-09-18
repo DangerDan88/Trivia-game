cdd3b63102aff86cd71ef0952a2e398b65af3327
Danger <51889207+DangerDan88@users.noreply.github.com>
2019-09-18 15:40:54 -0600

    seecond commit need to add timer and results function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,117 @@
- var quizContainer = $("#gameContainer");
- var resultsContainer = $("#results");
-var startButton = document.getElementById("start");
-var myQuestions = [
-    {
-        question: "Which NBA franchise has the most championships?",
-        answers: {
-          a: "Lakers",
-          b: "Jazz",
-          c: "Celtics"
-        } , 
-        
-    },
-    {
-        correctAnswer: "c"
-    }
+$(document).ready(function () {
+    var myQuestions = [
+        {
+            question: "Which NBA franchise has the most championships?",
+            answers: [
+                "Lakers",
+                "Jazz",
+                "Celtics",
+                "Wizards",
 
-]
+            ],
+            correctAnswer: "Celtics"
+        },
 
-window.onload = function() {
-   document.getElementById("start").addEventListener("click", function() {
+        {
+            question: "Who has scored the most points in a game in NBA history?",
+            answers: [
+                "Devin Booker",
+                "Kobe Bryant",
+                "Wilt Chamberlain",
+                "Michael Jordan",
+
+            ],
+            correctAnswer: "Wilt Chamberlain"
+        },
+
+        {
+            question: "What team did Magic Johnson play for?",
+            answers: [
+                "The Bucks",
+                "The jazz",
+                "The Lakers",
+                "The suns",
+
+            ],
+            correctAnswer: "The Lakers"
+        },
+
+        {
+            question: "How many points is the most Kobe Bryant scored in a game?",
+            answers: [
+                "77",
+                "65",
+                "80",
+                "81",
+
+
+            ],
+            correctAnswer: "81"
+        }
+
+
+
+    ]
+    var counter = 35;
+    var currentQuestion = 0;
+    var score = 0;
+    var losses = 0;
+    var timer;
+
+
+
+
+
+    function makeQuiz() {
+        document.getElementById("start").addEventListener("click", function () {
+
+
+
           
-   })
 
+            displayButton();
+            
+
+        })
+
+
+
+    }
+    makeQuiz();
+    function displayButton() {
     
+        
+        $("#timer").append(counter);
+        $("#gameContainer").empty();
+        var question = myQuestions[currentQuestion].question;
+        $("#gameContainer").append("<div>" + question + "</div>");
+        for (i = 0; i < myQuestions[currentQuestion].answers.length; i++) {
+            
+                $("#gameContainer").append("<button id='answerButton'>"+ myQuestions[currentQuestion].answers[i] + "</button>");
+            }
+            currentQuestion++;
+    }
+    document.getElementById("gameContainer").addEventListener("click", function (event) {
+        if(event.target.id === "answerButton") {
+            if (event.target.innerText === myQuestions[currentQuestion-1].correctAnswer) {
+                score++;
+                if (currentQuestion < 4){
+                displayButton();
+                }
+            } 
+            else {
+                losses++;
+                if(currentQuestion < 4) {
 
-  }
+                
+                displayButton();
+                }
+            }      
+        } 
+           
   
-  
+
+    })
+
+})
+
